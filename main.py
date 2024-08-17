478ea167817d2e01478b69a74bc8550a4194257f
WolfJack24 <liamschroder1@gmail.com>
2024-08-17 17:10:21 +0200

    Stop running the app as it has a infinite main loop

--- main.py
+++ main.py
@@ -14,8 +14,6 @@ from customtkinter import (
 )
 from assets.modules.List import List
 
-TEST = True
-
 
 class Gui(CTk):
     def __init__(self):
@@ -45,9 +43,6 @@ class Gui(CTk):
             text="Translate", font=self.source_code_pro,
             command=self.translate)
         self.submit_button.place(x=82, y=111)
-
-        if TEST:
-            self.destroy()
 
     def load_morse_code(self, morse_code: Any) -> None:
         self.morse_code = morse_code
